3b8057dea6bf6007abf9552bbe496220e4db5c56
Александр Бородич <saniainf@hotmail.com>
2021-03-24 16:46:43 +0700

    изменение провайдера базы данных на SQLOLEDB

--- TypographySearch/DBUtils.cs
+++ TypographySearch/DBUtils.cs
@@ -6,8 +6,8 @@ namespace TypographySearch
     class DBUtils
     {
         // @"Provider=SQLNCLI10.1;Data Source=LIDER1C;Persist Security Info=True;UID=sa;PWD=123;Initial Catalog=Typography_2020";
-
-        public string Provider { get; set; } = "SQLNCLI10.1";
+        // Provider=SQLOLEDB;Data Source=LIDER1C;Persist Security Info=True;User ID=sa;Initial Catalog=Typography_2021
+        public string Provider { get; set; } = "SQLOLEDB";
         public string Datasource { get; set; } = "LIDER1C";
         public string Database { get; set; } = "Typography_2021";
         public string Username { get; set; } = "sa";
@@ -19,8 +19,8 @@ namespace TypographySearch
             connectionString.Append("Provider=" + Provider + ";");
             connectionString.Append("Data Source=" + Datasource + ";");
             connectionString.Append("Initial Catalog=" + Database + ";");
-            connectionString.Append("UID=" + Username + ";");
-            connectionString.Append("PWD=" + Password + ";");
+            connectionString.Append("User ID=" + Username + ";");
+            connectionString.Append("Password=" + Password + ";");
             connectionString.Append("Persist Security Info=True;");
 
             OleDbConnection connection = new OleDbConnection(connectionString.ToString());
